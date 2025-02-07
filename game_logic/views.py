a430f1d7db2b3cdc541f506305e20c26eda2f5d3
Yuxuan Du <aimeedu@usc.edu>
2025-02-06 21:00:56 -0800

    implement fetch word_list

--- game_logic/views.py
+++ game_logic/views.py
@@ -1,48 +1,37 @@
 import json
+import random
 
 from django.http import JsonResponse
 from .db import db_ref
-from game_logic.game_logic import TypeGame
 
 # just for testing, need to delete when hosting
 from django.views.decorators.csrf import csrf_exempt
 
-try:
-    game = TypeGame(db_ref)
-    game.start_game()
-
-except ValueError as e:
-    game = None
-    error_message = str(e)
-except Exception as e:
-    game = None
-    error_message = f"Unexpected error during game setup: {str(e)}"
-
 
 # just for testing, need to delete when hosting
 @csrf_exempt
-def index(request):
+def get_word_list(request):
     if request.method == 'POST':
-        data = json.loads(request.body).get('user_input', None)
+        level = json.loads(request.body).get('level', None)
+        num = int(json.loads(request.body).get('num', None))
 
-        if data:
+        if level and num:
             try:
-                response_data = game.process_user_input(data)
-                print(response_data)
-                return JsonResponse(response_data, status=200)
+                path = f"levels/level{level}/words"
+                words = db_ref.child(path).get()
+
+                if not words:
+                    raise ValueError(f"No words available for Level {level}.")
+
+                word_list = random.sample(words, min(num, len(words)))
+                print(f"Words loaded for Level {level}: {word_list}")
+
+                return JsonResponse(word_list, status=200, safe=False)
             except ValueError as e:
                 return JsonResponse({'error': f'Value error from database: {str(e)}'}, status=400)
             except Exception as e:
                 return JsonResponse({'error': f'Unexpected error: {str(e)}'}, status=500)
         else:
-            return JsonResponse({'error': 'No user input provided'}, status=400)
-
-    return JsonResponse({'message': 'Please send a POST request'}, status=405)
-
-
-@csrf_exempt
-def get_word_list(request):
-    if request.method == 'POST':
-        return JsonResponse(game.word_list, status=200, safe=False)
+            return JsonResponse({'error': 'No parameter provided'}, status=400)
 
     return JsonResponse({'message': 'Please send a POST request'}, status=405, safe=False)
